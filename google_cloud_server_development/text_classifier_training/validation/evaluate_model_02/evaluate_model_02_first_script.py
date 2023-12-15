736b977340b1d7aa8bb5fb29107a942d366d8aeb
LucileTech <lucile.t.tech@gmail.com>
2023-12-15 09:11:04 -0800

    scripts for model evaluation

---
+++ google_cloud_server_development/text_classifier_training/validation/evaluate_model_02/evaluate_model_02_first_script.py
@@ -0,0 +1,33 @@
+import csv
+import json
+
+# Function to extract tweet content from JSONL file
+def extract_tweet_content(jsonl_file, output_csv):
+    with open(jsonl_file, 'r', encoding='utf-8') as infile, open(output_csv, 'w', newline='', encoding='utf-8') as outfile:
+        writer = csv.writer(outfile)
+        writer.writerow(['ID', 'Tweet Content', 'Threshold', 'Label'])  # Header for the CSV file
+
+        for line_number, line in enumerate(infile, start=1):
+            try:
+                # Replace single quotes with double quotes
+                line = line.replace("'", "\"")
+                # Parse as JSON after replacing quotes
+                data = json.loads(line)
+
+                if 'tweet content' in data:
+                    tweet_id = data['id']
+                    tweet_content = data['tweet content']
+                    confidences = data['prediction']['confidences']
+
+                    threshold = 0.75
+                    label = '1' if confidences[0] > threshold else '0' if confidences[1] > threshold else 'threshold too high'
+
+                    writer.writerow([tweet_id, tweet_content, threshold, label])
+            except json.JSONDecodeError as e:
+                print(f"Error in line {line_number}: {e}")
+                continue
+
+# Replace 'all_predictions.jsonl' with your JSONL file name
+# Replace 'tweet_contents.csv' with the desired output CSV file name
+extract_tweet_content('all_prediction_test.jsonl', 'tweet_contents.csv')
+
