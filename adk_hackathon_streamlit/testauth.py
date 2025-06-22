ab39c9f9423f15af30f62b1f46b178a4cf35f2da
CorinneEmakia <93287324+CorinneEmakia@users.noreply.github.com>
2025-06-21 17:57:36 -0700

    Delete adk_hackathon_streamlit/testauth.py

--- adk_hackathon_streamlit/testauth.py
+++
@@ -1,9 +0,0 @@
-from google.cloud import bigquery
-from google.oauth2 import service_account
-
-creds = service_account.Credentials.from_service_account_file("/Users/corinnedavid/Downloads/emakia-722bcde763ae.json")
-client = bigquery.Client(credentials=creds, project=creds.project_id)
-
-# Quick test
-for dataset in client.list_datasets():  
-    print(f"Found dataset: {dataset.dataset_id}")
