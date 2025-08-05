a2853e8d9c36b3152d736f6874608151d995048b
CorinneEmakia <93287324+CorinneEmakia@users.noreply.github.com>
2025-08-04 23:33:16 -0700

    Add files via upload

---
+++ apple_development/Enaelle/Enaelle/Enaelle/EnaelleApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnaelleApp.swift
+//  Enaelle
+//
+//  Created by Corinne David on 7/19/25.
+//
+
+import SwiftUI
+
+@main
+struct EnaelleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ apple_development/Enaelle/Enaelle/EnaelleTests/EnaelleTests.swift
@@ -0,0 +1,17 @@
+//
+//  EnaelleTests.swift
+//  EnaelleTests
+//
+//  Created by Corinne David on 7/19/25.
+//
+
+import Testing
+@testable import Enaelle
+
+struct EnaelleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
