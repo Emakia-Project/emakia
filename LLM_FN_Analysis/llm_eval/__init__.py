86dda16eb52cc897628137aa7f096d1fd52d262a
CorinneEmakia <93287324+CorinneEmakia@users.noreply.github.com>
2025-09-12 22:05:27 -0700

    Delete LLM_FN_Analysis directory

--- LLM_FN_Analysis/llm_eval/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-LLM Evaluation Package
-
-This package provides functionality for evaluating LLM performance on various tasks.
-"""
-
-from .evaluateold import evaluate_with_llm, batch_evaluate
-
-__all__ = ['evaluate_with_llm', 'batch_evaluate']
